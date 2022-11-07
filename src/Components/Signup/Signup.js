82524f9f7632401fdd3e33fc868a135929b1d9ac
sourov <sourov.jsr98@gmail.com>
2022-11-08 00:13:41 +0600

    signup created

--- src/Components/Signup/Signup.js
+++ src/Components/Signup/Signup.js
@@ -1,9 +1,62 @@
 import React from 'react';
+import signupimg from '../assets/images/signupimg.jpg'
 
 const Signup = () => {
-    return (
-        <div>
 
+    const handelSignup = (event) => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        const name = form.name.value;
+        const photo = form.photo.value;
+        console.log(email, password, name, photo)
+
+
+    }
+
+
+
+    return (
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col lg:flex-row">
+                <div className="text-center lg:text-left">
+                    <img src={signupimg} alt="" />
+                </div>
+                <div className="card flex-shrink-0 w-full max-w-md shadow-2xl bg-base-100">
+                    <form onSubmit={handelSignup} className="card-body">
+                        <h1 className='text-3xl'>Signup Now</h1>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Name</span>
+                            </label>
+                            <input type="text" name='name' placeholder="name" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Photo</span>
+                            </label>
+                            <input type="text" name='photo' placeholder="photo url" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="text" name='email' placeholder="email" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" name='password' placeholder="password" className="input input-bordered" />
+
+                        </div>
+                        <div className="form-control mt-6">
+                            <button type='submit' className="btn btn-primary">Signup</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
