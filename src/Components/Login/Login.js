545e955c265dcb9ba549ea61dcd0911336ad93ae
sourov <sourov.jsr98@gmail.com>
2022-11-08 00:04:19 +0600

    login page created

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,9 +1,47 @@
 import React from 'react';
-
+import loginimg from '../assets/images/loginimg.jpg'
 const Login = () => {
-    return (
-        <div>
 
+    const handelLogin = (event) => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password)
+
+
+    }
+
+
+
+    return (
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col lg:flex-row">
+                <div className="text-center lg:text-left">
+                    <img src={loginimg} alt="" />
+                </div>
+                <div className="card flex-shrink-0 w-full max-w-md shadow-2xl bg-base-100">
+                    <form onSubmit={handelLogin} className="card-body">
+                        <h1 className='text-3xl'>Login Now</h1>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="text" name='email' placeholder="email" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" name='password' placeholder="password" className="input input-bordered" />
+
+                        </div>
+                        <div className="form-control mt-6">
+                            <button type='submit' className="btn btn-primary">Login</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Components/Shared/Header.js
+++ src/Components/Shared/Header.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 const Header = () => {
     const menuItems = <>
         <li><Link to='/'>Home</Link></li>
+        <li><Link to='/services'>Services</Link></li>
         <li><Link to='/login'>Login</Link></li>
         <li><Link to='/signup'>Signup</Link></li>
     </>
